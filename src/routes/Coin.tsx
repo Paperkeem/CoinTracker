91df734b2049cab55de88f508467af3b66353fc6
Paperkeem <ahharlawhddl@gmail.com>
2023-01-31 01:21:12 +0900

    chore: setting

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Coin() {
+  return (
+    <div>
+      Coin detail
+    </div>
+  );
+}
+
---
+++ src/routes/Router.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import Coin from './Coin';
+import Coins from './Coins';
+import NotFound from './NotFound';
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { path: "", element: <Coins />},
+      { path: ":coinId", element: <Coin />},
+    ],
+  },
+]);
